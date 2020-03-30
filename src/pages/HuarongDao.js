e3a5f9952f57b0273449cdf534511391bc75abf5
hijackhi77 <legojackliu@gmail.com>
2020-03-29 23:18:45 -0400

    Update HuarongDao

--- src/pages/HuarongDao.js
+++ src/pages/HuarongDao.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Header from "../components/Header";
 import Game from "../components/Game";
 import Footer from "../components/Footer";
-import "./HuarongDao.css";
 
 class HuarongDao extends Component {
   render() {

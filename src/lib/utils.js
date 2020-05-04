157638a4ba7a2b71d4dd883c0164e92f7a709860
hijackhi77 <legojackliu@gmail.com>
2020-05-04 05:32:06 -0400

    Format code for utils; upload Huarong Dao background image

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,28 +1,23 @@
 const deepcopy = (object) => {
-    return JSON.parse(JSON.stringify(object));
+  return JSON.parse(JSON.stringify(object))
 }
 
 const getRandomInt = (max) => {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max))
 }
 
 const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  return new Promise((resolve) => setTimeout(resolve, milliseconds))
 }
 
 const flatten = (twoDimensionalArray) => {
-    let seq = [];
-    for (let i=0; i<twoDimensionalArray.length; ++i) {
-        for (let j=0; j<twoDimensionalArray[i].length; ++j) {
-            seq.push(twoDimensionalArray[i][j]);
-        }
+  let seq = []
+  for (let i = 0; i < twoDimensionalArray.length; ++i) {
+    for (let j = 0; j < twoDimensionalArray[i].length; ++j) {
+      seq.push(twoDimensionalArray[i][j])
     }
-    return seq;
+  }
+  return seq
 }
 
-export {
-    deepcopy,
-    getRandomInt,
-    sleep,
-    flatten
-};
+export { deepcopy, getRandomInt, sleep, flatten }
